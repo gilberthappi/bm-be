1893377f4c77aa820b5aa2970fe754672ea790fc
gilberthappi <gdushimimana6@gmail.com>
2024-10-02 20:26:08 +0300

    Add files and folders for authantication

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,10 @@
 import express from 'express';
 import userRouter from './authRoute';
-import caseRouter from './caseRoute';
 import contactRouter from './contactRoute';
-import blogRoute from './blogRoute';
-import followUpRouter from './followUpRoutes';
 
 const mainRouter = express.Router();
 
 mainRouter.use('/user', userRouter);
-mainRouter.use('/case',caseRouter);
 mainRouter.use('/cont',contactRouter);
-mainRouter.use('/blog',blogRoute);
-mainRouter.use('/follow',followUpRouter);
 
 export  default mainRouter;
